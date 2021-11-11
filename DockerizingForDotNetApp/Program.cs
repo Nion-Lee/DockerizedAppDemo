245e4ddcceb969344c100df4d38d080340fa9bca
冠龍 李 <nionismine@yahoo.com.tw>
2021-11-11 11:32:46 +0800

    Add project files.

---
+++ DockerizingForDotNetApp/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.Write("Please enter your name: ");
+var name = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine($"Hello, {name}.");
+Console.WriteLine("Welcome to Docker!");
